45939b2f6d661da08bf01689324cdb338e0034a1
TrustedSec <davek@trustedsec.com>
2014-10-08 12:30:13 -0400

    Initial release of hash_parser

--- hash_parser.py
+++ hash_parser.py
@@ -36,7 +36,7 @@ except IndexError:
 
 Written by: David Kennedy (ReL1K) @HackingDave from https://www.trustedsec.com
 
-This is a hash parser that will export a rc file compatible with Metasploit. This is useful when compromising a seperate domain and want to see if any of the credentials work on another domain or other systems.
+This is a hash parser that will export a rc file compatible with Metasploit. This is useful when compromising a separate domain and want to see if any of the credentials work on another domain or other systems.
 
 The first input is the filename that contains the hashes ex: Admin:500:LM:NTLM.
 The second input is the remote IP address you want to use smb_logins on to validate if the creds work.
